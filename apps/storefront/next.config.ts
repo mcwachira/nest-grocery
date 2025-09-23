e68fab26dab83d732179f5638875333510fa4418
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-09-23 16:15:52 +0300

    Finished setting up the app using a monorepo and made the taiwindcss work

--- apps/storefront/next.config.ts
+++ apps/storefront/next.config.ts
@@ -1,12 +1,20 @@
 import { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+    typescript: {
+        ignoreBuildErrors: true,
+    },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "images.unsplash.com", // Allow images from all domains
+            },
+        ],
+    },
 };
 
 export default nextConfig;
--- apps/storefront/src/app/page.tsx
+++ apps/storefront/src/app/page.tsx
@@ -1,6 +1,5 @@
 import { log } from "@repo/logger";
-import { Link } from "@repo/ui/link";
-import { CounterButton } from "@repo/ui/counter-button";
+
 
 export const metadata = {
   title: "Store | Kitchen Sink",
@@ -10,22 +9,10 @@ export default function Store() {
   log("Hey! This is the Store page.");
 
   return (
-    <div className="container">
-      <h1 className="title">
-        Store <br />
-        <span>Kitchen Sink</span>
-      </h1>
-      <CounterButton />
-      <p className="description">
-        Built With{" "}
-        <Link href="https://turborepo.com" newTab>
-          Turborepo
-        </Link>
-        {" & "}
-        <Link href="https://nextjs.org/" newTab>
-          Next.js
-        </Link>
-      </p>
-    </div>
+   <div className="text-center">
+       <h1 className="text-5xl text-blue-800">
+           Hello
+       </h1>
+   </div>
   );
 }
