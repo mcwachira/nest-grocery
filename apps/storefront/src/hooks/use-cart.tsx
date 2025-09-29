d5246e0bbc92ca8e378f189b742f4aa6a57304cc
mcwachira <23430963+mcwachira@users.noreply.github.com>
2025-09-29 12:26:51 +0300

    Created the use-Cart Hook

---
+++ apps/storefront/src/hooks/use-cart.tsx
@@ -0,0 +1,92 @@
+
+"use client"
+import React, { createContext, useContext, useState, ReactNode } from 'react';
+import {Product} from "@/src/lib/types.ts";
+
+
+export interface CartItem extends Product {
+    quantity: number;
+}
+
+interface CardContextType {
+    cartItems: CartItem[];
+    addToCart: (product: Product) => void;
+    removeFromCart: (productId: string) => void;
+    updateQuantity: (productId: string, quantity: number) => void;
+    clearCart: () => void;
+    getCartTotal: () => number;
+    getCartCount: () => number;
+}
+
+const CartContext = createContext<CardContextType | undefined>(undefined)
+
+export const CardProvider = ({children}:{children:ReactNode}) => {
+
+    const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+    const addToCart = (product:Product) => {
+        setCartItems((prev) => {
+            const existingItem = prev.find(item => item.id === product.id);
+
+            if(existingItem){
+                return prev.map((item) => item.id === product.id ? {...item, quantity: item.quantity + 1} : item);
+            }
+
+            return [...prev, {...product, quantity: 1}]
+        })
+    }
+    
+    const removeFromCart = (productId:string) => {
+        setCartItems((prev) => prev.filter((item) => item.id === productId))
+    }
+
+    const updateQuantity = (productId: string, quantity: number) => {
+        if (quantity <= 0) {
+            removeFromCart(productId);
+            return;
+        }
+
+        setCartItems(prev =>
+            prev.map(item =>
+                item.id === productId ? { ...item, quantity } : item
+            )
+        );
+    };
+
+    const clearCart = () => {
+        setCartItems([])
+    }
+
+    const getCartTotal = () => {
+        return cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+    }
+
+    const getCartCount = () => {
+        return cartItems.reduce((count, item) => count + item.quantity, 0);
+    };
+
+
+ return (
+    <CartContext.Provider
+        value={{
+            cartItems,
+            addToCart,
+            removeFromCart,
+            updateQuantity,
+            clearCart,
+            getCartTotal,
+            getCartCount,
+        }}
+    >
+        {children}
+    </CartContext.Provider>
+);
+};
+
+export const useCart = () => {
+    const context = useContext(CartContext);
+    if (!context) {
+        throw new Error('useCart must be used within a CartProvider');
+    }
+    return context;
+}
